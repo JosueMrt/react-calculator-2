305f830cceef7a057f6a8be334a61553e25cde25
axe i-grec <tag4rce7@gmail.com>
2020-04-29 11:25:00 +0200

    Done CSS

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,7 +3,8 @@ import '../App.css'
 
 const Display = ({ display, displayBis }) => {
   return (
-    <div id="total-display">
+    <div id="display-container">
+      <div id="both-displays"></div>
       <h3 id="display-bis">{displayBis}</h3>
       <h1 id="display">{display}</h1>
     </div>
